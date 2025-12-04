610f273a68dcb5b9ee50162ae6df13bbbe522cac
103031430 <103031430@student.swin.edu.au>
2025-12-04 12:15:50 +1100

    add code for connection to the agent

---
+++ src/custom_transport.h
@@ -0,0 +1,28 @@
+#include <Ethernet.h>
+
+struct micro_ros_agent_locator {
+	IPAddress address;
+	int port;
+};
+
+extern void* transport_args;
+
+static inline void set_microros_eth_transports(byte mac[], IPAddress client_ip, IPAddress agent_ip, uint16_t agent_port){
+
+	static struct micro_ros_agent_locator locator;
+
+   	Ethernet.begin(mac, client_ip);
+	delay(1000);
+
+	locator.address = agent_ip;
+	locator.port = agent_port;
+
+	rmw_uros_set_custom_transport(
+		false,
+		&locator,
+		platformio_transport_open,
+		platformio_transport_close,
+		platformio_transport_write,
+		platformio_transport_read
+	);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <Arduino.h>
-#include <SPI.h>
+
 #include <Ethernet.h>
+#include <EthernetUdp.h>
+#include <Udp.h>
+#include <SPI.h>
+
+#include "mros_platformio.h"
+
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#include <std_msgs/msg/int32.h>
+
+EthernetUDP udp;
 
 #define W5500_CS    14    // CS (Chip Select) PIN
 #define W5500_RST   9     // Reset PIN (optional)
@@ -10,19 +23,15 @@
 #define W5500_SCK   13    // Serial Clock PIN
 
 // Define MAC address
-byte mac[] = { 0xDE, 0xAD, 0xAF, 0x91, 0x3E, 0xD7 };
+byte mac[] = { 0xDE, 0xAD, 0xAF, 0x91, 0x3E, 0xD7 };    // Mac address of ESP32
+IPAddress client_ip(192, 168, 1, 200);                  // IP address of ESP32
+IPAddress agent_ip(192, 168, 1, 10);                    // IP address of Micro ROS agent        
+size_t agent_port = 8888;                               // Agent Port Number
 
-// Static IP Configuration
-IPAddress ip(192, 168, 1, 200);         // Static IP address
-IPAddress dns(0, 0, 0, 0);              // DNS server
-IPAddress gateway(192, 168, 1, 1);      // Gateway address
-IPAddress subnet(255, 255, 255, 0);     // Subnet mask
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-
-  while (!Serial) {}      // Waits until Serial Monitor is open
 
   delay(1000);
   Serial.println("Starting Ethernet Connection... ");
@@ -33,36 +42,11 @@ void setup() {
   // Select CS PIN and initialize Ethernet with static IP settings (Selecting CS PIN required for ESP32 as the ardiuno default is different)
   Ethernet.init(W5500_CS);
 
-  // Start the Ethernet Connection
-  Ethernet.begin(mac, ip, dns, gateway, subnet);
+  set_microros_eth_transports(mac, client_ip, agent_ip, agent_port);
 
-  // Check if the Ethernet Hardware is detected
-  Serial.println("Checking Ethernet hardware...");
-  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-    Serial.println("ERROR: No Ethernet hardware detected!");
-    return;
-  } else {
-    Serial.println("Ethernet hardware detected!");
-  }
-
-  // Check if the IP addressing is properly assigned
-    if (Ethernet.localIP() == IPAddress(0, 0, 0, 0)) {
-    Serial.println("Failed to configure Ethernet with static IP");
-    while (true); // Halt on failure
-  }
-
-  // Check if the cable is connected to the ESP and link is validate.
-  if (Ethernet.linkStatus() == LinkOFF) {
-    Serial.println("Link is OFF. Check cable connection.");
-  } else {
-    Serial.println("Link is ON. Check cable connection.");
-    Serial.print("To test connection, Please ping: ");
-    Serial.print(Ethernet.localIP());
-  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(1000);
+
 
 }
